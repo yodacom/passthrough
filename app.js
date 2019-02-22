4ca711de239e04eeb185713c57fcdaead1efe3fe
Jeremy Black <jb@yodacom.com>
2019-02-22 09:33:02 -0700

    updated jade files to pug, added pug package, updated debug ddue to security warnings

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 //set up cors
 app.use(cors());
@@ -38,14 +38,14 @@ app.use('/users', users);
 app.use('/beers', beers);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
